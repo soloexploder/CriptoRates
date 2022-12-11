aff20a0705f6770f70c1e541a43184f1165cc1c6
soloexploder <soloexploder@gmail.com>
2022-12-11 21:05:55 +0300

    Initial Commit

---
+++ CriptoRates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CriptoRates
+//
+//  Created by Даниил Хантуров on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
